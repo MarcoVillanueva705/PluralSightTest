87b866c79ce2bc9e71994cd05f87be937d48f9ee
marco villanueva <villanuevamarco98@gmail.com>
2020-11-03 20:19:15 -0700

    two things in classes

--- PluralSightTest/Program.cs
+++ PluralSightTest/Program.cs
@@ -5,6 +5,14 @@ namespace PluralSightTest
     class Program
     {
         static void Main(string[] args)
+            //classes consist of two things: the state or the data it holds,
+            //and the behavior, which acts on the state
+
+        //what we want to do with Class Book is I want it to hold the grades
+        //that are coming into the application, and then I want to provide the behavior
+        //that will act on those grades, so behaviors that will allow adding grades to the 
+        //book and behaviors that will allow computing of statistics for all the grades
+        //that the book is holding
         {
             Console.WriteLine("Hello World!");
         }
